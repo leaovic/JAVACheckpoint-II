822e5a2143cfb9b32886b5a66b6aa61279f7629a
Victor <leaovictor@live.com>
2022-07-06 14:06:38 -0300

    Criado uma pasta "entities" e criado uma classe "Program".

---
+++ src/entities/Program.java
@@ -0,0 +1,14 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-aa");
+    }
+}
